5217124fb6dcaccd5e4deae98656bd6553a144d9
Lexi Allen <lexi.allen14@gmail.com>
2024-02-19 12:32:16 -0700

    Server Tested and Runs

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+
+const app = express ();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get ('/test', (req,res) => {
+    res.send('Server is up and running');
+});
+
+app.listen(PORT, ()=> {
+    console.log("Server is running on port", PORT);
+})
