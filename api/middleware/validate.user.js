6ccabeb289484511b5b313015488ff9419690ff7
Lexi Allen <lexi.allen14@gmail.com>
2024-02-19 19:31:42 -0700

    updated server

---
+++ api/middleware/validate.user.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.model')
+
+const validateUser = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+  
+
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+    req.user = user
+
+    return next()
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+module.exports = validateUser
--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const cors = require ('cors');
 const app = express ();
 
 const PORT = process.env.PORT || 3000;
+const DB_NAME = process.env.DB_NAME
+const DB_URL = process.env.DB_URL
+
+const EXPRESS_API = express()
+mongoose.connect(DB_URL + DB_NAME)
+const DB = mongoose.connection
+
 
 app.use(express.json());
 
